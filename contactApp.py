99e74b51deff145c382c523f3511357d2261ebcf
Nadder <129917079+Nadder28@users.noreply.github.com>
2023-05-02 23:15:22 -0400

    created a user class that writes and reads to and from a json file, as well as I created the initialization of the Contact class

--- contactApp.py
+++ contactApp.py
@@ -1,6 +1,7 @@
 import tkinter
 import sqlite3
 import re
+import json
 
 class Application():
     """Class to create the gui
@@ -14,26 +15,40 @@ class Application():
 class User:
     """Defines the user account with a login information and the corresponding address book for the account
     """
-    def __init__(self, username, password):
+    def __init__(self):
         pass
+    def login(self, username, password):
+        """Checks to see if username and password match to any existing user"""
+        f = open('users.json', 'r')
+        data = json.loads(f.read())
+        for login in data['logins']:
+            if login['username'] == username and login['password'] == password:
+                return login
+        return None
+        
     
-    def login():
-        """Checks to see if username and password match to any existing user
-        """
-        pass
-    
-    def signUp():
+    def signUp(self, username, password):
         """Registers the login information into database unless username already exists
         """
-        pass
-    
-    pass
+        with open('users.json') as f:
+            data = json.loads(f.read())
+        newUser = {"username": username, "password": password, "addressBook": []}
+        for login in data['logins']:
+            if login['username'] == username:
+                return None
+        data['logins'].append(newUser)
+        with open('users.json', 'w') as f:
+           json.dump(data,f, indent=2)
 
 class Contact:
     """Defines an individual contact and the details that are included in the contact
     """
     def __init__(self, FirstName, LastName, number, address, email):
-        pass
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.number = number
+        self.address = address
+        self.email = email
     
     def add_contact():
         pass
